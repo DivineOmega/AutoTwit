0c7ed7fb8794d6263b90781b9d74b4ae4322d63e
Jordan Hall <jordan.hall@rapidweb.biz>
2016-04-12 14:20:29 +0100

    Prevent regular tweets from mentioning users

--- src/main/Main.java
+++ src/main/Main.java
@@ -232,7 +232,7 @@ public class Main
 				
 				if (allTweets.contains(sentence) || sentence.length()>140 || sentenceLower.contains("…") 
 						|| sentenceLower.contains("\r") || sentenceLower.contains("\n") 
-						|| sentenceLower.startsWith("rt ")) continue;
+						|| sentenceLower.contains("@") || sentenceLower.startsWith("rt ")) continue;
 
 				boolean sentenceContainsExclusion = false; 
 				
